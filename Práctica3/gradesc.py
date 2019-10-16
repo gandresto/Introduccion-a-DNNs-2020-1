28fdf44a218e5d016f33a198e1cd3dbc9dd475b0
Andrés González <gandresto@outlook.com>
2019-10-15 22:56:11 -0500

    Corrijo erroresen función de costo

--- Práctica3/gradesc.py
+++ Práctica3/gradesc.py
@@ -1,12 +1,11 @@
-import numpy as np
-
+#import numpy as np
 def h(x, theta=1):
     return x*theta
 
 def fc_min_cuadrados(xvect = [], yvect=[], theta=1):
     J = 0
     for x, y in zip(xvect, yvect):
-        J = (h(x, theta)-y)**2
+        J += (h(x, theta)-y)**2
     J /= 2*len(xvect)
     return J
 
