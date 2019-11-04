16c24652df6365f5fb40b378534d03e32a750409
Andrés González <gandresto@outlook.com>
2019-11-03 23:13:57 -0600

    Corrijo valores de targets

--- Práctica4/p4ej2.py
+++ Práctica4/p4ej2.py
@@ -4,27 +4,28 @@ import matplotlib.pyplot as plt
 
 p_vect = [[1,1], [1,0], [1,2], [5,1], [0,-1], [-1,0], [-2,-1], [-3,-2]]
 p_vect = [np.matrix(p) for p in p_vect]
-t_vect = [-1,-1,-1,-1,1,1,1,1]
+t_vect = [0,0,0,0,1,1,1,1]
 
 w = np.matrix(np.random.rand(2))
-b = np.matrix(1)
+b = np.matrix(np.random.rand(1))
 
-epochs = 20
+lr = 0.5
+epochs = 3
 e_vect = []
 
 for epoch in range(epochs):
     for i, p in enumerate(p_vect):
-        #print('Para el punto p = ', p)
+        # print('Para el punto p = ', p)
         a = per.afn(p, w, b)
-        #print('a = ', a)
+        # print('a = ', a)
         e = per.efn(t_vect[i], a)
         e_vect.append(e)
-        #print('e = ', e)
-        w, b = per.reglaApr(w, b, e, p)
-        #print('w = ', w)
-        #print('b = ', b)
-        if i % 8 == 0:
-            print("Epoca %d, Error = %d" % (epoch, e))
+        # print('e = ', e)
+        w, b = per.reglaApr(w, b, e, p, lr)
+        # print('w = ', w)
+        # print('b = ', b)
+        if i % 4 == 0:
+            print("Epoca %d (%i/%i), Error = %d" % (epoch, i, len(p_vect), e))
 
 plt.plot(e_vect)
 plt.title('Error vs iteraciones')
