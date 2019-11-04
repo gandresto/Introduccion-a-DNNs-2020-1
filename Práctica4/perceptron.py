63a8ff35417f9fd49869e14a336880e632f5b65f
Andrés González <gandresto@outlook.com>
2019-11-03 23:13:18 -0600

    Agregar learning rate a función de aprendizaje

--- Práctica4/perceptron.py
+++ Práctica4/perceptron.py
@@ -1,15 +1,15 @@
 import numpy as np
 
 def hardlim(x):
-    return 1 if x>=0 else 0
+    return 1 if x>0 else 0
 
 def afn(x, w, b):
-    return hardlim(w*x.T+b)
+    return hardlim(np.dot(w, x.T)+b)
 
 def efn(t, a):
     return t-a
 
-def reglaApr(w_ant, b_ant, e, p):
-    w_nuevo = w_ant + e*p
-    b_nuevo = b_ant + e
+def reglaApr(w_ant, b_ant, e, p, lr=1):
+    w_nuevo = w_ant + lr*e*p
+    b_nuevo = b_ant + lr*e
     return w_nuevo, b_nuevo
